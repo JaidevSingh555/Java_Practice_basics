1a3a2c03474089c124d4eb21d63e657e2f7c06ac
Jaidev Singh <jaidevsingh555@gmail.com>
2025-03-17 13:59:02 +0530

    Updated code with latest changes

--- ManyPrimes.java
+++ ManyPrimes.java
@@ -2,10 +2,11 @@ public class ManyPrimes {
     public static void main(String[] args) {
         int n = 500;
         System.out.println();
-        int count = 0, loopcount = 0;
-        top: for (int i = 2; i <= n; i++) {
+        System.out.print("2,");
+        int count = 1, loopcount = 0;
+        top: for (int i = 3; i <= n; i += 2) {
             int limit = (int) Math.sqrt(i);
-            for (int j = 2; j <= limit; j++) {
+            for (int j = 3; j <= limit; j += 2) {
                 loopcount++;
                 if (i % j == 0) {
                     continue top;
@@ -14,6 +15,6 @@ public class ManyPrimes {
             System.out.print(i + ",");
             count++;
         }
-        System.out.println("Count"+" "+count+" "+"LoopCount"+" "+loopcount);
+        System.out.println("Count" + " " + count + " " + "LoopCount" + " " + loopcount);
     }
 }
